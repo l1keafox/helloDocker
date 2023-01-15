79cffe1a1e3f250e41dfb750cf93be7c9e3cff10
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-15 11:48:06 -0700

    Persistant DB/sharing app/udpatin gapp

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # helloDocker
+
+
+This is my first docker app, and following the todo app in getting started.
+
+Downloaded the app.zip and unpacked and created dockerfile to start.
--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">You have no todo items yet! Add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
@@ -175,5 +175,7 @@ function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
         </Container>
     );
 }
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
